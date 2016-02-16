576002bf114fea1ab7b09484a83c08e50751f2cd
Andra <andrazaharia@ymail.com>
2016-02-16 18:17:24 +0000

    Flask Microblogging Server

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir, ADMINS, MAIL_PORT, MAIL_SERVER, MAIL_PASWORD, MAIL_USERNAME
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASWORD)
+    mail_handler = STMPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failurie', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
